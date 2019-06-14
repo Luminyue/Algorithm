f088cf2ff5ff3c5da90ba7b9bc0e22cb0328af51
hezhengqiao <1829007308@qq.com>
2019-06-14 18:05:14 +0800

    插入排序

--- app/src/main/java/net/lzzy/algorithm/MainActivity.java
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.security.PrivateKey;
 import java.util.Calendar;
 import java.util.Random;
 
@@ -35,7 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 displayItems(edtItems);
                 break;
             case R.id.activity_main_btn_sort:
-                directSort();
+//                directSort();
+                insertSort();
                 displayItems(tvResult);
                 break;
             default:
@@ -56,22 +58,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //todo:直接选择排序的具体实现
         //选择排序
 
-        for (int i = 0;i<items.length-1;i++){
-            int minPos=i;
-            for (int j=i+1;j<items.length;j++){
-                if (items[minPos].compareTo(items[j])>0){
-                    minPos=j;
-                    //12156
+        for (int i = 0; i < items.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < items.length; j++) {
+                if (items[minPos].compareTo(items[j]) > 0) {
+                    minPos = j;
                 }
             }
-            swap(minPos,i);
+            swap(minPos, i);
+        }
+    }
+
+    private void insertSort () {
+        for (int i = 1; i < items.length; i++) {
+            if (items[i] < items[i - 1]) {
+                int temp = items[i];
+                int k = i - 1;
+                for (int j = k; j >= 0 && temp < items[j]; i--) {
+                    items[j + i] = items[j];
+                    k--;
+                }
+                items[k + 1] = temp;
+            }
         }
     }
 
     private void swap(int m, int n) {
-        int tmp=items[m];
-        items[m]=items[n];
-        items[n]=tmp;
+        int tmp = items[m];
+        items[m] = items[n];
+        items[n] = tmp;
     }
 
 
