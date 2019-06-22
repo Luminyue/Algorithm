0c8247270c9eb2364e5be029a2c998673c677cdd
hezhengqiao <1829007308@qq.com>
2019-06-22 15:31:58 +0800

    希尔排序

--- app/src/main/java/net/lzzy/algorithm/BaseSort.java
+++ app/src/main/java/net/lzzy/algorithm/BaseSort.java
@@ -11,50 +11,57 @@ public  abstract class BaseSort<T extends Comparable<?super T>> {
     private int swapCount;//移动次数
     int moveStep;//交换次数
 
-    //endregi
-    public BaseSort(T[] items) {
+    BaseSort(T[] items) {
         this.items = items;
         compareCount = 0;
-        duration=0;
+        duration = 0;
         swapCount = 0;
         moveStep = 0;
     }
+
     boolean bigger(T a, T b) {
         compareCount++;
         return a.compareTo(b) > 0;
     }
-    void swap(int i, int j){
-        T num=(T)items[i];
+
+    void swap(int i, int j) {
+        T num = (T) items[i];
         items[i] = items[j];
         items[j] = num;
         swapCount++;
     }
-
-    public abstract void sort();
-    public void sortwithtime(){
-        long start=System.currentTimeMillis();
+    public String getResult() {
+        String display = " ";
+        for (T i : items) {
+            display = display.concat(i + ",");
+        }
+        display = display.substring(0, display.length() - 1);
+        return display;
+    }
+    public void sortwithtime() {
+        long start = System.currentTimeMillis();
         sortwithtime();
-        duration=System.currentTimeMillis()-start;
+        duration = System.currentTimeMillis() - start;
 
     }
-    public long getDuration () {
+
+    abstract void sort();
+
+    public long getDuration() {
         return duration;
     }
-    public int getCompareCount () {
+
+    public int getCompareCount() {
         return compareCount;
     }
+
     public int getSwapCount() {
         return swapCount;
     }
+
     public int getMoveStep() {
         return moveStep;
     }
-    public String getResult(){
-        String display=" ";
-        for (T i:items) {
-            display=display.concat(i+",");
-        }
-        display=display.substring(0,display.length()-1);
-        return display;
-    }
+
+
 }
